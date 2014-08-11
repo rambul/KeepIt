0854767aaeece64c3457533f1c51e25a42a219f5
Ram Bulatnikov <ramb@adextent.com>
2014-08-12 02:40:58 +0300

    added support for multiple albums + albums.json, added facebook connect

--- init.js
+++ init.js
@@ -1,33 +1,45 @@
 	$(document).ready(function(){
 
-		$.getJSON("test-json/album.json", function(data) {
-		  }).done(function(d) {
+		var albums;
 
-		  	var numberOfPhotos;
-
-		  	album = d;
-
-		    var albumLink = album.link;
-		    var albumName  = album.name;
-		    var photos = album.photos;
-
-		    numberOfPhotos = photos.length;
-
-		    $("#album-selection").append("<h2>" + albumName + "</h2><a href="+albumLink+" target=\"_blank\">View Album</a><br/><br/>");
-
-		    var albumHtml = "<ul class=\"photos\">";
-
-		    for(i=0;i<numberOfPhotos;i++){
-		    	currentPhoto = photos[i];
-		    	console.log(currentPhoto);
-		    	albumHtml += "<li><img src=\"" + currentPhoto.picture + "\" /><br/><a target=\"_blank\" href=\"" + currentPhoto.images[0].source + "\">View Full Size</a><br/></li>";
-		    }
-
-		    albumHtml += "</ul>";
-
-		    console.log(albumHtml);
-
-		    $("#album-selection").append(albumHtml);
-
+		$.getJSON("test-json/albums.json",function(data) {}).done(function(d) {
+			albums = d;
+			console.log(d);
+			for(i=0;i<albums.length;i++){
+				getAlbum(albums[i].url);
+				console.log(albums[i].url);
+			}
 		});
+
+		function getAlbum(url){
+			console.log(url);
+			$.getJSON(url, function(data) {
+			  }).done(function(d) {
+
+			  	var numberOfPhotos;
+
+			  	album = d;
+
+			    var albumLink = album.link;
+			    var albumName  = album.name;
+			    var photos = album.photos;
+
+			    numberOfPhotos = photos.length;
+
+			    $("#album-selection").append("<h2>" + albumName + "</h2><a href="+albumLink+" target=\"_blank\">View Album</a><br/><br/>");
+
+			    var albumHtml = "<ul class=\"photos\">";
+
+			    for(i=0;i<numberOfPhotos;i++){
+			    	currentPhoto = photos[i];
+			    	albumHtml += "<li><img src=\"" + currentPhoto.picture + "\" /><br/><a target=\"_blank\" href=\"" + currentPhoto.images[0].source + "\">View Full Size</a><br/></li>";
+			    }
+
+			    albumHtml += "</ul>";
+
+			    $("#album-selection").append(albumHtml);
+
+			});
+		}
+
 	});
