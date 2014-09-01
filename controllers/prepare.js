5c1a6fbe59a66e71f984b2f7b1e8887919fd54ba
Ram Bulatnikov <ramb@adextent.com>
2014-09-02 00:37:20 +0300

    htaccess fix, template #2

--- controllers/prepare.js
+++ controllers/prepare.js
@@ -2,7 +2,7 @@ app.controller('prepare', function($scope,sharedProperties) {
 	var chosenPhotos = sharedProperties.getObject();
 
 	$("#album-preview").addClass("template-1");
-	
+
 	chosenPhotos.each(function(){
 		$("#album-preview").append("<li>");
 		$("#album-preview li:last").append($(this).find("img"));
