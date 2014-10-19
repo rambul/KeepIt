2b33a038b2c6338380763b89a19ef7b190d6c74e
Ram Bulatnikov <ramb@adextent.com>
2014-10-20 01:51:07 +0300

    flipbook changes

--- controllers/flipbook.js
+++ controllers/flipbook.js
@@ -1,20 +1,21 @@
 app.controller('flipbook', function($scope,sharedProperties) {
 
 	var chosenPhotos = sharedProperties.getObject();
-	var flipbook = $(".magazine");
+	var flipbook = $("#flipbook");
 
-	flipbook.addClass("template-1");
+	flipbook.append("<div class=\"hard\"></div>");
 
 	chosenPhotos.each(function(){
-		flipbook.append("<div>");
-		flipbook.append($(this).find("img"));
+		flipbook.append("<div style=\"background-image:url("+$(this).find("img").attr("src")+"\"><img src=\""+$(this).find("img").attr("src")+"\"></div>");
 	});
 
 
 	flipbook.turn({
-		width: "100%",
-		height: "70%",
-		autoCenter: true
+			width:'100%',
+			height:600,
+			elevation: 50,
+			gradients: true,
+			autoCenter: true
 	});
 
 });
